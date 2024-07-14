97c5dde110e6026e4c0c7492bfa39d653090060f
khaerul12 <khaerulusman68@gmail.com>
2024-07-15 00:23:21 +0700

    2024-07-13 10:29 pm

---
+++ phpdasar.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 14, 2024 at 07:22 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpdasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nrp` char(9) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `jurusan` varchar(64) NOT NULL,
+  `gambar` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `email`, `jurusan`, `gambar`) VALUES
+(1, '20201001', 'Budi Santoso', 'budi.santoso@example.com', 'Teknik Informatika', 'budi.jpg'),
+(2, '20201002', 'Ayu Lestari', 'ayu.lestari@example.com', 'Sistem Informasi', 'ayu.jpg'),
+(3, '20201003', 'Rizki Pratama', 'rizki.pratama@example.com', 'Teknik Elektro', 'rizki.jpg'),
+(5, '20201005', 'Sigit Nugroho', 'sigit.nugroho@example.com', 'Akuntansi', 'sigit.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
